8f43c785e9f46a4928a2c73f93155e37b5898bf8
David <david_borelli@live.com>
2020-05-08 00:19:38 -0300

    ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => res.json({ message: "Hello World" }));
+app.get('/', (req, res) => res.json({ message: 'Hello World' }));
 
-app.listen(3333, () => console.log("🚀 Server no ar na porta 3333!"));
+app.listen(3333, () => console.log('🚀 Server no ar na porta 3333!'));
