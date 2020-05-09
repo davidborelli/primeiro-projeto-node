b1d147a801cd2fd6dd4a0064a5b2618e37c32a84
David <david_borelli@live.com>
2020-05-09 12:33:51 -0300

    Trabalahndo com dados, DTO

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,6 +1,12 @@
 import Appointment from '../models/Appointment';
 import { isEqual } from 'date-fns';
 
+// DTO = Data Transfer Object
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
 class AppointmentsRepository {
   private appointments: Appointment[];
 
@@ -15,8 +21,8 @@ class AppointmentsRepository {
     return findedAppointment || null;
   }
 
-  public create(provider: string, date: Date): Appointment {
-    const appointment = new Appointment(provider, date);
+  public create({ provider, date }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
 
     this.appointments.push(appointment);
     return appointment;
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -24,7 +24,10 @@ apponintmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appointment is already booked' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   response.json(appointment);
 });
